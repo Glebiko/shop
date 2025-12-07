b7224fae0609951bd8653c6fc1041647b16719c7
Glebiko <kiddylove96@gmail.com>
2025-12-07 11:31:56 +0200

    added page reviews

--- app/js/main.js
+++ app/js/main.js
@@ -7,3 +7,17 @@ const swiper = new Swiper('.accessories__slider', {
     prevEl: '.accessories__arrow-prev',
   },
 });
+
+const swiperReviews = new Swiper('.reviews__slider', {
+  loop: true,
+  slidesPerView: 12,
+  spaceBetween: 16,
+  pagination: {
+    el: ".reviews__pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: '.reviews__arrow-next',
+    prevEl: '.reviews__arrow-prev',
+  },
+});
